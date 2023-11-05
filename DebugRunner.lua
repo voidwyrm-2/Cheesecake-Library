d87b067bcbe3b36325fbb4615aaa8cec5198b293
Radioactivity <85001747+voidwyrm-2@users.noreply.github.com>
2023-11-05 14:52:34 -0500

    Update DebugRunner.lua

--- DebugRunner.lua
+++ DebugRunner.lua
@@ -1,1 +1,26 @@
+--[[
+#include "CheesecakeLib.lua"
+]]
 
+do
+ TestTest(true)
+ TestMathandNumLett()
+ TestEquals()
+ TestDefault()
+ TestClamp()
+end
+
+function TestTest(prnt)
+ a = 'Hey ya!'
+ if prnt then
+  print(a)
+ end
+end
+
+function TestMathandNumLett()
+ a = 7
+ b = 6
+ loop = 0
+ while loop ~= 
+ Math()
+end
