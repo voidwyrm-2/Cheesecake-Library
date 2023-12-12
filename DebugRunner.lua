e843ec567cb78e1a851f4585dae3de23393612dd
Radiating Fettuccine <85001747+voidwyrm-2@users.noreply.github.com>
2023-12-12 15:23:24 -0500

    Update DebugRunner.lua

--- DebugRunner.lua
+++ DebugRunner.lua
@@ -1,27 +1,10 @@
---[[
-#include "CheesecakeLib.lua"
-]]
-
 do
  TestTest(true)
- TestMathandNumLett()
- TestEquals()
- TestDefault()
- TestClamp()
 end
 
 function TestTest(prnt)
  a = 'Hey ya!'
  if prnt then
   print(a)
- end
-end
-
-function TestMathandNumLett()
- a = 7
- b = 6
- loop = 0
- while loop ~= 7
-  Math()
  end
 end
